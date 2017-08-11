aecffc3feb9b5a3f89253665814d81e2fa08509b
heenadhawan <angelheena06@gmail.com>
2017-08-11 11:59:53 +0530

    CRUD on Cart

---
+++ onlineshopping/src/main/java/net/kzn/onlineshopping/service/CartService.java
@@ -0,0 +1,91 @@
+package net.kzn.onlineshopping.service;
+
+import java.util.List;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import net.kzn.onlineshopping.model.UserModel;
+import net.kzn.shoppingbackend.dao.CartLineDAO;
+import net.kzn.shoppingbackend.dto.Cart;
+import net.kzn.shoppingbackend.dto.CartLine;
+import net.kzn.shoppingbackend.dto.Product;
+
+@Service("cartService")
+public class CartService {
+
+	@Autowired
+	private CartLineDAO cartLineDAO;
+	
+	
+	@Autowired
+	private HttpSession session;
+	
+	// returns the cart of the user who has logged in
+	private Cart getCart() {
+		
+		return ((UserModel)session.getAttribute("userModel")).getCart();
+		
+	}
+	
+	// returns the entire cart lines
+	public List<CartLine> getCartLines() {
+		return cartLineDAO.list(this.getCart().getId());		
+	}
+
+	public String updateCartLine(int cartLineId, int count) {
+
+		// fetch the cart line
+		CartLine cartLine = cartLineDAO.get(cartLineId);
+		
+		if(cartLine == null) {
+			return "result=error";
+		}
+		else {
+			
+			Product product = cartLine.getProduct();
+			double oldTotal = cartLine.getTotal();
+			if(product.getQuantity() <= count) {
+				count = product.getQuantity();
+			}
+			cartLine.setProductCount(count);
+			cartLine.setBuyingPrice(product.getUnitPrice());
+			cartLine.setTotal(product.getUnitPrice() * count);
+			cartLineDAO.update(cartLine);
+			Cart cart = this.getCart();
+			cart.setGrandTotal(cart.getGrandTotal() - oldTotal + cartLine.getTotal());
+			cartLineDAO.updateCart(cart);
+			
+			return "result=updated";
+		}
+	}
+	
+       public String deleteCartLine(int cartLineId) {
+		//fetch the cart lines
+    	   CartLine cartLine = cartLineDAO.get(cartLineId);
+    	   if(cartLine == null){
+    		   return "result=error";
+    		   
+    	   }
+    	   else{
+    		   Cart cart = this.getCart();
+    		   cart.setGrandTotal(cart.getGrandTotal()-cartLine.getTotal());
+    		   cart.setCartLines(cart.getCartLines()-1);
+    		   cartLineDAO.updateCart(cart);
+    		   //remove the cart
+    		   cartLineDAO.delete(cartLine);
+    		   return"result=deleted";
+    		   
+    		   
+    	   } 
+       }   
+    		   
+}   
+    		   
+    		   
+    		   
+    	   
+	
+	
--- shoppingbackend/src/main/java/net/kzn/shoppingbackend/dto/Cart.java
+++ shoppingbackend/src/main/java/net/kzn/shoppingbackend/dto/Cart.java
@@ -1,74 +1,75 @@
 package net.kzn.shoppingbackend.dto;
 
-import java.util.List;
+import java.io.Serializable;
 
-import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
 @Entity
-public class Cart{
+public class Cart implements Serializable {
+	
 	/**
 	 * 
 	 */
-	
+	private static final long serialVersionUID = 1L;
+
 	/*
 	 * private fields
 	 * */
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	
-	@OneToMany(mappedBy = "cart" , cascade=CascadeType.ALL , fetch = FetchType.EAGER)
-	private List<CartItem> cartItems;
-
-	
-	private double grandTotal;
-
+	/*--------*/
 	@OneToOne
-	@JoinColumn(name = "user_id")
 	private User user;
-	
-			
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public double getGrandTotal() {
-		return grandTotal;
-	}
-
-	public void setGrandTotal(double grandTotal) {
-		this.grandTotal = grandTotal;
-	}
-
+		
 	public User getUser() {
 		return user;
 	}
-
-	public void setCustomer(User user) {
+	public void setUser(User user) {
 		this.user = user;
 	}
 
-	public List<CartItem> getCartItems() {
-		return cartItems;
-	}
+	/*--------*/
 
-	public void setCartItems(List<CartItem> cartItems) {
-		this.cartItems = cartItems;
-	}
 
+
+	@Column(name = "grand_total")	
+	private double grandTotal;
+	@Column(name = "cart_lines")
+	private int cartLines;
 	
-
-		 
+	/*
+	 * setter and getters for the fields
+	 * */
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public double getGrandTotal() {
+		return grandTotal;
+	}
+	public void setGrandTotal(double grandTotal) {
+		this.grandTotal = grandTotal;
+	}
+	public int getCartLines() {
+		return cartLines;
+	}
+	public void setCartLines(int cartLines) {
+		this.cartLines = cartLines;
+	}
+	
+	
+	@Override
+	public String toString() {
+		return "Cart [id=" + id + ", grandTotal=" + grandTotal + ", cartLines=" + cartLines + "]";
+	}
+	
 }
